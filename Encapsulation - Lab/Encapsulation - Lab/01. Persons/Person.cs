7ab2362729237e2b6b4a5addf98d562a787e96e8
Spasina Spasova <75899012+SpasinaSpasova@users.noreply.github.com>
2021-02-27 10:30:28 +0200

    Encapsulation - Lab

---
+++ Encapsulation - Lab/Encapsulation - Lab/01. Persons/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        public Person(string first, string second, int personAge)
+        {
+            this.FirstName = first;
+            this.LastName = second;
+            this.Age = personAge;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+        }
+
+
+
+    }
+}
---
+++ Encapsulation - Lab/Encapsulation - Lab/02. Salary/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+        public Person(string first, string second, int personAge, decimal personSalary)
+        {
+            this.FirstName = first;
+            this.LastName = second;
+            this.Age = personAge;
+            this.Salary = personSalary;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public decimal Salary { get; set; }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age < 30)
+            {
+                percentage /= 2;
+            }
+            this.Salary = this.Salary + this.Salary * percentage / 100;
+        }
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.";
+        }
+
+
+
+    }
+}
